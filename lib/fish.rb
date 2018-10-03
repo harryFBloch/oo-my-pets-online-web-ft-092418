6f4c1978cef321e9b516acab5b022e055a82c6e4
Harry Bloch <harryfbloch@gmail.com>
2018-10-03 20:04:58 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,4 +1,14 @@
 class Fish
   attr_accessor :mood
   attr_reader :name
+  
+   def initialize(name)
+    self.name = name
+    self.mood = "nervous"
+  end
+  
+  private
+  def name=(name)
+    @name = name
+  end
 end
